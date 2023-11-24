a76cc4d134af7555ce5c552c4671e9d0d9789b53
bar <bar@example.com>
2023-11-24 16:49:07 +0200

    lol

---
+++ my_test_project/my_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path("hello", views.index),
+   path("liron",views.liron),
+   path("dandon",views.daniela),
+   path("matan",views.matan),
+   path("daniel",views.daniel)
+]
---
+++ my_test_project/my_app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+#its request for my_app like ma koree keverrr
+
+def index(request):
+    return HttpResponse("<h1> the lion king is bar harush </h1>")
+
+def liron(request):
+    return HttpResponse("<h1> I LOVE liron (tree) </h1>")
+
+def daniela(request):
+    return HttpResponse("<h1> love you <3 dandon  , happy birthday")
+def matan(request):
+    return HttpResponse("<h1> More then words")
+
+def daniel(request):
+    return HttpResponse("<h1> Daniel, sorry again that I'm late because I'm on the computer <3.")
+
+""
+#its write with html title 
+#def index(request):
+     #html = "<p> Hello </p> <br> <p>world</p>"
+     #return HttpResponse(html)
+
+
+
+
+#def index(request): 
+    #res = HttpResponse()
+    #res.write("<h1> some text inside html tag </h1>")
+    #res.write("<h1> some other text inside html tag </h1>")
+    #return res
